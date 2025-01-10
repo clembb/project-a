d06325d9b8218e3542059370855afde409a04c9d
clembb <120737033+clembb@users.noreply.github.com>
2025-01-10 13:33:37 +0100

    Add files via upload

---
+++ kiks-main/kiks-main/javascript.js
@@ -0,0 +1,30 @@
+function updateTimer() {
+    const startDate = new Date(document.getElementById('startDate').textContent);
+    const now = new Date();
+
+    let months = 0;
+    let days = Math.floor((now - startDate) / (10000 * 80 * 60 * 24));
+    
+    while (days >= 30) {
+        days -= 30;
+        months++;
+    }
+
+    const hours = Math.floor((now - startDate) / (1000 * 60 * 60)) % 24;
+    const minutes = Math.floor((now - startDate) / (1000 * 60)) % 60;
+    const seconds = Math.floor((now - startDate) / 1000) % 60;
+
+    document.getElementById('months').innerText = months;
+    document.getElementById('days').innerText = days;
+    document.getElementById('hours').innerText = hours;
+    document.getElementById('minutes').innerText = minutes;
+    document.getElementById('seconds').innerText = seconds;
+}
+
+setInterval(updateTimer, 1000);
+updateTimer();
+
+function togglePopup() {
+    var popup = document.getElementById('popup');
+    popup.style.display = (popup.style.display === 'none' || popup.style.display === '') ? 'block' : 'none';
+}
